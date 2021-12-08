4c76d727a5567d9289e21908cc5edb0b3640e503
TimNiklasWitte <wittet@uos.de>
2021-12-08 15:58:27 +0100

    finished LSTM cell

--- Homework 07/LSTM_Cell.py
+++ Homework 07/LSTM_Cell.py
@@ -2,6 +2,51 @@ import tensorflow as tf
 
 class LSTM_Cell:
 
-    # units = hidden size
+    # units = hidden size = size of hidden state vector #= The length of the resulting vector (similar to the units argument in Dense layers)
+    # 
     def __init__(self,units):
-        self.dense_layer_hstate = tf.keras.layers.Dense(units, activation=None, use_bias=False)
+
+        # Forget Gate
+        self.dense_layer_forget = tf.keras.layers.Dense(units, activation="sigmoid", bias_initializer=tf.keras.initializers.Ones())
+
+        # Input Gate
+        self.dense_layer_input = tf.keras.layers.Dense(units, activation="sigmoid")
+
+        # Cell-state Candiates
+        self.dense_layer_candiates = tf.keras.layers.Dense(units, activation="tanh") # <- Not sigmoid
+
+        # Output Gate
+        self.dense_layer_output = tf.keras.layers.Dense(units, activation="sigmoid")
+        
+
+    def call(self, x, states):
+
+        cell_state, hidden_state = states
+
+        concated = tf.concat((states, x), axis= 1)
+
+        # 1. Preparing
+
+        # Forget Gate
+        f_t = self.dense_layer_forget(concated)
+
+        # Input Gate
+        i_t = self.dense_layer_input(concated)
+
+        # Cell-state Candiates
+        ĉ_t = self.dense_layer_candiates(concated)
+        
+        # 2. Update Cell State
+        cell_state = tf.math.multiply(f_t, cell_state) + tf.math.multiply(i_t, ĉ_t)
+
+        # 3. Determining hidden state/output
+        # Output gate
+        o_t = self.dense_layer_output(concated)
+
+        # New hidden state
+        regularized_cell_state = tf.math.tanh(cell_state)
+        hidden_state = tf.math.multiply(o_t, regularized_cell_state)
+
+        print(concated)
+
+        return cell_state, hidden_state
--- Homework 07/Training.py
+++ Homework 07/Training.py
@@ -1,28 +1,57 @@
 import numpy as np
+import tensorflow as tf
+
+from LSTM_Cell import *
+
+seq_len = 10
+num_samples = 100
 
 RANGE_MAX = 2
 RANGE_MIN = -RANGE_MAX
 
+def prepare_data(data):
+
+    #cache this progress in memory, as there is no need to redo it; it is deterministic after all
+    data = data.cache()
+
+    #shuffle, batch, prefetch
+    data = data.shuffle(1350)
+    data = data.batch(32)
+    data = data.prefetch(20)
+    #return preprocessed dataset
+    return data
 
 def main():
-    data = next(integration_task(10, 100))
-    print(data)
 
+    # dataset = tf.data.Dataset.from_generator(my_integration_task, (tf.float32, tf.uint8))# , output_signature=tf.TensorSpec(shape,dtype))
     
+    # dataset = dataset.apply(prepare_data)
+ 
+    # train_dataset = dataset.take(1350)
+    # test_dataset = dataset.take(180)
+
+    # train_dataset = dataset.take(1)
+    # for elem in train_dataset:
+    #     print(elem)
+
 
 def integration_task(seq_len, num_samples):
-
     for _ in range(num_samples):
         data = np.random.uniform(RANGE_MIN, RANGE_MAX, seq_len)
         data = np.expand_dims(data,-1)
         sum = np.sum(data)
 
         if sum >= 1:
-            yield (data, 1)
+            return data, 1
         else:
-            yield (data, 0)
+            return data, 0
 
 
+def my_integration_task():
+    global seq_len, num_samples
+    for _ in range(80000):
+        data, target = integration_task(seq_len, num_samples)
+        yield data, target
 
 if __name__ == "__main__":
     try:
