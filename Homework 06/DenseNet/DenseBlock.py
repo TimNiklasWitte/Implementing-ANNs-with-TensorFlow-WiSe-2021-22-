a73ab63c8c3d5b3e9996779633dcc8106a655d40
Tobias Kretschel <tkretschel@gmail.com>
2021-12-08 14:09:18 +0100

    :label: added type hints

--- Homework 06/DenseNet/DenseBlock.py
+++ Homework 06/DenseNet/DenseBlock.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 class DenseBlock(tf.keras.layers.Layer):
 
     def __init__(self, n_filters, new_channels):
@@ -23,18 +24,18 @@ class DenseBlock(tf.keras.layers.Layer):
             tf.keras.layers.Activation(tf.nn.relu),
 
             # 1x1 conv -> No Padding ("valid") needed
-            tf.keras.layers.Conv2D(n_filters, kernel_size=(1,1), padding="valid", use_bias=False),
+            tf.keras.layers.Conv2D(n_filters, kernel_size=(1, 1), padding="valid", use_bias=False),
 
             tf.keras.layers.BatchNormalization(epsilon=1.001e-05),
             tf.keras.layers.Activation(tf.nn.relu),
 
             # to be concatenated with the input
-            tf.keras.layers.Conv2D(new_channels, kernel_size=(3,3), padding="same", use_bias=False),
+            tf.keras.layers.Conv2D(new_channels, kernel_size=(3, 3), padding="same", use_bias=False),
         ]
 
     @tf.function
-    def call(self, x, train):
-        
+    def call(self, x: tf.Tensor, train: bool) -> tf.Tensor:
+
         """
         Propagate the input towards all layers
 
@@ -47,15 +48,11 @@ class DenseBlock(tf.keras.layers.Layer):
 
         # Propagte towards all layers
         for layer in self.layer_list:
-            
+
             # Replay train parameter to BatchNormalization layers during call
             if isinstance(layer, tf.keras.layers.BatchNormalization):
                 x = layer(x, training=train)
             else:
                 x = layer(x)
 
-        return tf.keras.layers.Concatenate(axis=-1)([input, x]) 
-
-    
-
-    
+        return tf.keras.layers.Concatenate(axis=-1)([input, x])
