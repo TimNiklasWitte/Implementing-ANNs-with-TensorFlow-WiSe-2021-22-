d4ba130e75c4096cbf339667597e4510c9b5c29f
TimNiklasWitte <wittet@uos.de>
2022-01-20 23:45:48 +0100

    SkipGram is now trainable

--- Homework 10/SkipGram.py
+++ Homework 10/SkipGram.py
@@ -2,11 +2,51 @@ import tensorflow as tf
 
 class SkipGram(tf.keras.layers.Layer):
      
-    def __init__(self):
-        pass 
+    def __init__(self, vocabulary_size, embedding_size):
+        super(SkipGram, self).__init__()
 
-    def build(self, input_shape):
-        pass
+        self.vocabulary_size = vocabulary_size
+        self.embedding_size = embedding_size
 
-    def call(self, x, training=False):
-        pass 
+        self.build(embedding_size) 
+
+        self.embedding = tf.keras.layers.Embedding(self.vocabulary_size, self.embedding_size)
+
+        learning_rate = 0.01
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate)
+
+
+    def build(self, embedding_size):
+
+        self.w_score = self.add_weight(
+            shape=(self.vocabulary_size, self.embedding_size), initializer="random_normal", trainable=True
+        )
+        self.b_score = self.add_weight(
+            shape=(self.embedding_size, 1), initializer="random_normal", trainable=True
+        )
+
+
+    def call(self, x, target):
+   
+        embedd = self.embedding(x)
+   
+        return tf.nn.nce_loss(
+            weights=self.w_score,               # [vocab_size, embed_size]
+            biases=tf.squeeze(self.b_score),    # [vocab_size]
+            labels=target,                      # [bs, 1]
+            inputs=embedd,                      # [bs, embed_size]
+            num_sampled=32,                     # negative sampling: number 
+            num_classes=self.vocabulary_size
+        )
+         
+
+    @tf.function
+    def train_step(self, input, target):
+        # loss_object and optimizer_object are instances of respective tensorflow classes
+        with tf.GradientTape() as tape:
+            loss = self(input, target)
+         
+
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        return loss
--- Homework 10/Training.py
+++ Homework 10/Training.py
@@ -1,7 +1,118 @@
+from glob import glob
 import tensorflow as tf
 import tensorflow_text as tf_txt
 
-contextWindowSize = 2
+import numpy as np
+
+import tqdm
+from SkipGram import *
+
+
+contextWindowSize = 4
+numSentences = 1000
+
+
+def main():
+    global vocabulary_size
+    sentencesData = loadSentencesData()
+    vocabulary_size = countWords(sentencesData)
+    
+
+    embedding_size = 64
+    NUM_EPOCHS = 10
+
+    skipGram = SkipGram(vocabulary_size, embedding_size)
+
+    dataset = tf.data.Dataset.from_generator(dataGenerator, (tf.int32, tf.int32))
+    dataset = dataset.apply(prepare_data)
+
+    train_size = 10
+    test_size = 10
+    train_dataset = dataset.take(train_size)
+
+    dataset.skip(train_size)
+    test_dataset = dataset.take(test_size)
+
+    file_path = "test_logs/test"
+    summary_writer = tf.summary.create_file_writer(file_path)
+    with summary_writer.as_default():
+        for epoch in range(NUM_EPOCHS):
+            print(f"Epoch: {epoch}")
+
+            epoch_loss_agg = []
+
+            for input, target in tqdm.tqdm(train_dataset,position=0, leave=True, total=train_size/32):
+       
+                target = tf.expand_dims(target, -1)
+                loss = skipGram.train_step(input, target)
+     
+                epoch_loss_agg.append(loss)
+            
+            train_loss = np.mean(epoch_loss_agg)
+           
+            tf.summary.scalar(name="Train loss", data=train_loss, step=epoch)
+         
+
+def loadSentencesData():
+    global contextWindowSize
+    global numSentences
+
+    file = open("bible.txt", "r")
+    data = file.read()
+
+    sentences = data.split("\n")
+    sentences = [s for s in sentences if s != ""]
+
+    sentences = [s.lower() for s in sentences]
+
+    sentences = [removeSpecialCharacters(s) for s in sentences]
+    
+    # filtering
+    sentences = [s for s in sentences if len(s.split(" ")) >= 2*contextWindowSize + 1]
+
+    sentences = sentences[:numSentences]
+
+    # map: word->number
+    word_dict = {}
+    wordId = 0
+    for sentence in sentences:
+        words = sentence.split(" ")
+        for word in words:
+            try:
+                word_dict[word] = word_dict[word] 
+            except:
+                word_dict[word] = wordId
+                wordId += 1
+       
+   
+
+    for idx, sentence in enumerate(sentences):
+        words = sentence.split(" ")
+        
+        mappedWords = []
+        for word in words:
+            mappedWords.append(word_dict[word])
+
+        sentences[idx] = mappedWords
+    
+
+    return sentences # = number of words
+
+def countWords(sentences):
+    global contextWindowSize
+
+    word_dic = {}   
+    for sentence in sentences:
+
+ 
+        for idx in range(2*contextWindowSize + 1):
+            word = sentence[idx]
+            try:
+                word_dic[word] += 1
+            except KeyError:
+                word_dic[word] = 1
+
+    return len(word_dic)
 
 def removeSpecialCharacters(string):
     result = ""
@@ -22,65 +133,28 @@ def removeSpecialCharacters(string):
 
     return result 
 
-def main():
-
-    # s = dataGenerator()
-    # s = next(s)
-    # print(s)
-    
-    dataset = tf.data.Dataset.from_generator(dataGenerator, (tf.string, tf.string))
-    dataset = dataset.take(100)
-
-    for data in dataset:
-        print(data)
-    #dataset = dataset.apply(prepare_data)
-
-
-
-    # splitter = tf_txt.RegexSplitter()
-    # splitter.split(test)
-
-    # tf.print(test)
-
 def dataGenerator():
     global contextWindowSize
 
-    file = open("bible.txt", "r")
-    data = file.read()
 
-    data = data.split("\n")
-    data = [s for s in data if s != ""]
-
-    data = [s.lower() for s in data]
-
-    data = [removeSpecialCharacters(s) for s in data]
+    sentencesData = loadSentencesData()
+ 
+    #print(vocabulary_size)
+    for encodedSentence in sentencesData:
     
-    data = data[10000:]
+        for idx in range(2*contextWindowSize + 1):
+            input_word = encodedSentence[contextWindowSize]
+            target = encodedSentence[idx]
+    
+            if idx != contextWindowSize:
+                yield (input_word, target) 
 
-    for s in data:
-
-        words = s.split(" ")
-        if len(words) == 2*contextWindowSize + 1:
-            
-            input_word = words[contextWindowSize]
-            
-            for idx, target in enumerate(words):
-                if idx != contextWindowSize:
-                    #print(input_word, target) 
-                    yield (input_word, target) 
-            
-            # print(s)
-            # print("-----")
 
 def prepare_data(data):
-
-    #convert data from uint8 to float32
-    #data = data.map(lambda img: tf.cast(img, tf.float32) )
-
-    #sloppy input normalization, just bringing image values from range [0, 255] to [-1, 1]
-    #data = data.map(lambda img: (img/128.)-1. )
-
-    #data = data.map(lambda img: tf.reshape(img, shape=(28,28,1)) )
+    global vocabulary_size
+    
+    #create one-hot inputs and targets
+    #data = data.map(lambda input, target: (tf.one_hot(input, depth=vocabulary_size), tf.one_hot(target, depth=vocabulary_size)))
 
     #cache this progress in memory, as there is no need to redo it; it is deterministic after all
     data = data.cache()
